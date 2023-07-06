477c5a3d0545a9940a6d94d52483a3bc186dee01
Kenyansa <kenyansajustine@gmail.com>
2023-07-06 13:35:24 +0300

    add queary selectors for input for cities and button

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const apiKey = "d73ca1ff266b81160a8f0222ece6da79";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=nairobi";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkweather(){
+const searchBox = document.querySelector(".search-box input");
+const searchBtn = document.querySelector(".search-box button");
+
+async function checkweather(city){
     try {
-        const response = await fetch(apiUrl + `&appid=${apiKey}`);
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
         if (!response.ok) {
           throw new Error('Request failed with status ' + response.status);
         }
@@ -14,9 +17,9 @@ async function checkweather(){
       }
 
       document.querySelector(".city").innerHTML = data.name; //adds city name
-      document.querySelector(".temp").innerHTML = data.main.temp; //adds temp, which is the api object is inside main object
-      document.querySelector(".humidity").innerHTML = data.main.humidity;//adds humidy, which is the api object is inside main object
-      document.querySelector(".wind").innerHTML = data.data.wind.speed;//adds humidy, which is the api object is inside main object
+      document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&degC"; //adds temp, which is the api object is inside main object
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";//adds humidy, which is the api object is inside main object
+      document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";//adds humidy, which is the api object is inside main object
 }
 
 checkweather();
