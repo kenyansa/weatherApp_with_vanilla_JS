1a8bb9cf2419608a427e485afa78a3368ca2f741
Kenyansa <kenyansajustine@gmail.com>
2023-07-06 12:47:23 +0300

    add error handling to the async function

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const apiKey = "d73ca1ff266b81160a8f0222ece6da79";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=nairobi";
+
+async function checkweather(){
+    try {
+        const response = await fetch(apiUrl + `&appid=${apiKey}`);
+        if (!response.ok) {
+          throw new Error('Request failed with status ' + response.status);
+        }
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log('An error occurred:', error);
+      }
+}
+
+checkweather();
